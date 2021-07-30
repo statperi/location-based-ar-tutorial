b09195e3cfec113503ca68234e9d0a84f92c41e5
Periklis <periklis.stathopoulos@voicesage.com>
2021-07-30 16:36:41 +0100

    test

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ var models = [
         scale: '0.1 0.1 0.1',
         rotation: '0 180 0',
         info: 'Articuno',
-        position: '0 10 0'
+        position: '0 20 0'
     },
     //{
     //    url: './assets/magnemite/scene.gltf',
