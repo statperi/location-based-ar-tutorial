9c6a24ec6f61ae184b8222888d2994cf6f5e3582
Periklis <periklis.stathopoulos@voicesage.com>
2021-10-07 16:55:43 +0100

    test iii

--- locationAR.js
+++ locationAR.js
@@ -2,61 +2,47 @@
 var ar_model;
 
 window.onload = () => {
-    //const button = document.querySelector('button[data-action="change"]');
-    //button.innerText = '﹖';
-
     ar_model = ar_models[0];
 
-    getPosition();
-
-    $('.model-selector').select2();
-    $('.model-selector').on('select2:select', function (e) {
-        var data = e.params.data;
-        ar_model = ar_models.find(function (m) { return m.code == data.id });
-
-        clearModels();
-        createModel(ar_model, position);
-    });
+    getCurrentLocation();
 };
+
+
+var ar_models = [{
+    code: 'articuno',
+    url: './assets/articuno/scene.gltf',
+    scale: '0.1 0.1 0.1',
+    rotation: '0 180 0',
+    info: 'Articuno'
+}, {
+    code: 'bird',
+    url: './assets/phoenix_bird/scene.gltf',
+    scale: '0.1 0.1 0.1',
+    rotation: '0 180 0',
+    info: 'Bird'
+}
+];
 
 
 var setModel = function (model, entity) {
     var element = $(entity);
 
-    if (model.scale) {
-        // entity.setAttribute('scale', model.scale);
-        element.attr('scale', model.scale);
-    }
-
-    if (model.rotation) {
-        //entity.setAttribute('rotation', model.rotation);
-        element.attr('rotation', model.rotation);
-    }
-
-    if (model.position) {
-        // entity.setAttribute('position', model.position);
-        element.attr('position', model.position);
-    }
-
-    //entity.setAttribute('gltf-model', model.url);
-    //entity.setAttribute('animation-mixer', '');
-
+    if (model.scale) element.attr('scale', model.scale);
+    if (model.rotation) element.attr('rotation', model.rotation);
+    if (model.position) element.attr('position', model.position);
+    
     element.attr('gltf-model', model.url);
     element.attr('animation-mixer', '');
-
-    const name = document.querySelector('.name');
-    modelName = name.innerText = model.info;
 };
 
 
-function getPosition() {
+function getCurrentLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(setPosition);
     } else {
         alert("Sorry, your browser does not support HTML5 geolocation.");
     }
 }
-
 
 function setPosition(location) {
     position = {
@@ -67,7 +53,7 @@ function setPosition(location) {
         }
     };
 
-    createModel(ar_model, position);
+    // createModel(ar_model, position);
 }
 
 
@@ -82,178 +68,31 @@ function createModel(model, place) {
 }
 
 
-function clearModels() {
-    var entities = $('a-entity[gps-entity-place]');
-    entities.remove();
+
+
+function getLocations(currentLocation) {
+
+    $.ajax({
+        url: 'https://4ov2cmmwri.execute-api.eu-west-1.amazonaws.com/Prod/api/coordinates',
+        headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+        crossDomain: true,
+        type: 'POST',
+        dataType: 'json',
+        data: currentLocation,
+        success: function (data) {
+            console.log(data);
+            getLocationSuccess(data);
+        },
+        error: function (err) {
+            console.log(err);
+            alert("Could not get target locations");
+        }
+    });
 }
 
 
-var ar_models = [
-    {
-        code: 'articuno',
-        url: './assets/articuno/scene.gltf',
-        scale: '0.1 0.1 0.1',
-        rotation: '0 180 0',
-        info: 'Articuno'
-    },
-    {
-        code: 'bird',
-        url: './assets/phoenix_bird/scene.gltf',
-        scale: '0.1 0.1 0.1',
-        rotation: '0 180 0',
-        info: 'Bird'
-    },
-    //{
-    //    code: 'bear',
-    //    url: './assets/bear/scene.gltf',
-    //    scale: '0.1 0.1 0.1',
-    //    rotation: '0 180 0',
-    //    info: 'Bear'
-    //},
-    //{
-    //    code: 'dragonite',
-    //    url: './assets/dragonite/scene.gltf',
-    //    scale: '0.5 0.5 0.5',
-    //    rotation: '0 180 0',
-    //    info: 'Dragonite'
-    //},
-];
-
-
-//function staticLoadPlaces() {
-//    return [
-//        {
-//            name: 'Voicesage',
-//            location: {
-//                lat: 53.300399,
-//                lng: -6.176398,
-//            },
-//        },
-//        {
-//            name: 'Home',
-//            location: {
-//                lat: 53.299684,
-//                lng: -6.177198,
-//            },
-//        },
-//        {
-//            name: 'Bear',
-//            location: {
-//                lat: 53.301403,
-//                lng: -6.177578,
-//            },
-//        },
-//        {
-//            name: 'Bear Stillorgan',
-//            location: {
-//                lat: 53.2889278,
-//                lng: -6.2071782,
-//            },
-//        },
-//        {
-//            name: 'Spire',
-//            location: {
-//                lat: 53.349810,
-//                lng: -6.260130,
-//            },
-//        },
-//        {
-//            name: 'Poolbeg Powerstation',
-//            location: {
-//                lat: 53.3402763,
-//                lng: -6.189487,
-//            },
-//        }
-//    ];
-//}
-
-
-
-//function renderPlaces(places) {
-//    let scene = document.querySelector('a-scene');
-
-//    //places.forEach((place) => {
-//    //    let latitude = place.location.lat;
-//    //    let longitude = place.location.lng;
-
-//    //    let model = document.createElement('a-entity');
-//    //    model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-
-//    //    setModel(models[modelIndex], model);
-
-//    //    scene.appendChild(model);
-//    //});
-
-
-//    //document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-//    //    var entities = document.querySelectorAll('a-entity[gps-entity-place]');
-
-//    //    modelIndex++;
-//    //    var newIndex = modelIndex % models.length;
-
-//    //    for (var i = 0; i < entities.length -1; i++) {
-//    //        setModel(models[newIndex], entities[i]);
-//    //    }
-//    //});
-
-
-//    //document.querySelector('button[data-action="search"]').addEventListener('click', function () {
-//    //    clearModels();
-
-//    //    let bird = models[1];
-//    //    let text = $('.searchtext')[0].value;
-//    //    let place = getPlace(text);
-
-//    //    createModel(bird, place);
-//    //});
-//}
-
-
-
-
-//var models = [
-//    {
-//        code: 'articuno',
-//        url: './assets/articuno/scene.gltf',
-//        scale: '0.1 0.1 0.1',
-//        rotation: '0 180 0',
-//        info: 'Articuno'
-//    },
-//    {
-//        code: 'bird',
-//        url: './assets/phoenix_bird/scene.gltf',
-//        scale: '0.1 0.1 0.1',
-//        rotation: '0 180 0',
-//        info: 'Bird'
-//    },
-//    {
-//        code: 'bear',
-//        url: './assets/bear/scene.gltf',
-//        scale: '0.1 0.1 0.1',
-//        rotation: '0 180 0',
-//        info: 'Bear'
-//    },
-//];
-
-
-//function getPlace(text) {
-//    let place;
-//    if (text.toUpperCase() === "Grattan House".toUpperCase()) {
-//        place = {
-//            name: 'Voicesage',
-//            location: { lat: 53.300399, lng: -6.176398 }
-//        }
-//    } else if (text.toUpperCase() === "Bear".toUpperCase()) {
-//        place = {
-//            name: 'Bear',
-//            location: { lat: 53.301403, lng: -6.177578 }
-//        }
-//    } else {
-//        place = {
-//            name: 'Home',
-//            location: { lat: 53.299684, lng: -6.177198 }
-//        }
-//    }
-//    return place;
-//}
-
+function getLocationSuccess(response) {
+    if (response.status == 200) {
+        console.log(response);
+    }
+}
